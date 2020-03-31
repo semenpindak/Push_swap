8dfeef11a2535c0c36e1d25511f6be3b4c3eef88
Semen Pindak <pindak@inbox.ru>
2020-03-31 15:01:20 +0300

    31.03

--- Makefile
+++ Makefile
@@ -1,23 +1,25 @@
-PROGRAM = checker
+PROGRAM = push_swap
 CFLAGS:= -Wall -Wextra -Werror
-SRCS = main.c
+SRCS =	main.c\
+		validation.c\
+		sorting.c
 OBJ = $(SRCS:c=o)
-
 
 .PHONY: all clean fclean re
 
 all: $(PROGRAM)
 $(PROGRAM): $(OBJ)
-	$(CC) $(CFLAGS) $(OBJ) -o $@
+	make -C libft/
+	${CC} $(CFLAGS) ${OBJ} -o $@ -L libft/ -lft
 
 %.o: %.c
-	$(CC) $(CFLAGS) -g -MD -c $<
+	${CC} ${CFLAGS} -g -MD -c $<
 include $(wildcard *.d)
 
 clean:
-	rm -rf $(OBJ)
+	rm -f $(OBJ) *.d .DS_Store && make clean -C libft/
 
 fclean: clean
-	rm -rf $(PROGRAM)
+	rm -f $(PROGRAM) && make fclean -C libft/
 
 re: fclean all
--- push_swap.h
+++ push_swap.h
@@ -3,19 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: calpha <calpha@student.42.fr>              +#+  +:+       +#+        */
+/*   By: semen <semen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 20:09:00 by calpha            #+#    #+#             */
-/*   Updated: 2020/02/14 12:59:29 by calpha           ###   ########.fr       */
+/*   Updated: 2020/03/31 14:27:48 by semen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include <stdarg.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
+# include "libft/libft.h"
+
+int		validation(int ac, char *av[]);
+void	sorting(int ac, char *av[]);
 
 #endif
